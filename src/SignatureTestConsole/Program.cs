32490ab35b00d817f7147afb92b816501c494a44
Madalin <schiopu_madalin@yahoo.com>
2017-01-09 12:02:22 +0200

    #sample project added

---
+++ src/SignatureTestConsole/Program.cs
@@ -0,0 +1,122 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Signatures;
+using System.Security.Cryptography.X509Certificates;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing.Layout;
+using System.IO;
+using System.Drawing;
+
+namespace TestConsole
+{
+    class Program
+    {
+
+        class SignAppearenceHandler : ISignatureAppearanceHandler
+        {
+            private XFont font = new XFont("Verdana", 10, XFontStyle.Regular);
+
+            public XImage Image = XImage.FromFile(@"..\..\resources\logo.jpg");
+            public void DrawAppearance(XGraphics gfx, XRect rect)
+            {
+                var backColor = XColor.Empty;
+                var defaultText = "Signed by Napoleon \nLocation: Paris \nDate: " + DateTime.Now.ToString();
+
+                XFont font = new XFont("Verdana", 7, XFontStyle.Regular);
+
+                XTextFormatter txtFormat = new XTextFormatter(gfx);
+
+                var ratio = Image.PixelWidth / Image.PixelHeight;
+
+                var currentPosition = new XPoint(0, 0);
+
+
+                if (this.Image != null)
+                {
+                    gfx.DrawImage(this.Image, currentPosition.X, currentPosition.Y, rect.Width / 4, rect.Width / 4 / ratio);
+                    currentPosition = new XPoint(rect.Width / 4, 0);
+                }
+
+                txtFormat.DrawString(defaultText,
+                    font,
+                    new XSolidBrush(XColor.FromKnownColor(XKnownColor.Black)),
+                    new XRect(currentPosition.X, currentPosition.Y, rect.Width - currentPosition.X, rect.Height),
+                    XStringFormats.TopLeft);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            CreateAndSign();
+            SignExisting();
+
+        }
+
+
+        private static void CreateAndSign()
+        {
+            string path = "CreateAndSign.pdf";
+            XFont font = new XFont("Verdana", 10, XFontStyle.Regular);
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            var rect = new XRect(0, 0, page.Width, page.Height);
+            gfx.DrawString("Sample document", font, XBrushes.Black, rect, XStringFormats.TopCenter);
+
+            var signatureOptions = new PdfSignatureOptions()
+            {
+                ContactInfo = "Contact Info",
+                Location = "Paris",
+                Reason = "Test signatures",
+                Rectangle = new XRect(36, 700, 200, 50)
+            };
+
+            PdfSignatureHandler signatureHandler = new PdfSignatureHandler(
+                GetCert(),
+                signatureOptions
+                );
+            signatureHandler.AttachToDocument(document);
+
+            document.Save(path);
+            Process.Start(path);
+        }
+
+
+        private static void SignExisting()
+        {
+            
+            string path = string.Format("SignExisting.pdf");
+
+            PdfDocument document = PdfReader.Open(new MemoryStream(Properties.Resources.doc1));
+
+            var signatureOptions = new PdfSignatureOptions()
+            {
+                ContactInfo = "Contact Info",
+                Location = "Paris",
+                Reason = "Test signatures",
+                Rectangle = new XRect(36, 735, 200, 50),
+                  
+                AppearanceHandler = new SignAppearenceHandler()
+            };
+
+            PdfSignatureHandler signatureHandler = new PdfSignatureHandler(GetCert(), signatureOptions);
+            signatureHandler.AttachToDocument(document);
+
+            document.Save(path);
+            Process.Start(path);
+        }
+
+        private static X509Certificate2 GetCert()
+        {            
+            return new X509Certificate2(@"C:\work\kds\svnroot\test-console\pdfs\MigraDoc\code\Demo\Demo_TemporaryKey.pfx", "qwe123");
+        }
+    }
+}
