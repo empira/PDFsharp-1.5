7dda0d4b2d764bb3f8ee17e7beb71eb31424dd08
TH-Soft <githubspam@hoevel.de>
2017-07-26 18:29:32 +0200

    Changes for 1.50 beta 4

--- src/PdfSharp/root/ProductVersionInfo.cs
+++ src/PdfSharp/root/ProductVersionInfo.cs
@@ -115,7 +115,7 @@ namespace PdfSharp
         /// <summary>
         /// The build number of the product.
         /// </summary>
-        public const string VersionBuild = "4000";  // V16G // Build = days since 2005-01-01  -  change this values ONLY HERE
+        public const string VersionBuild = "4589";  // V16G // Build = days since 2005-01-01  -  change this values ONLY HERE
 
         /// <summary>
         /// The patch number of the product.
@@ -125,7 +125,7 @@ namespace PdfSharp
         /// <summary>
         /// The Version Prerelease String for NuGet.
         /// </summary>
-        public const string VersionPrerelease = "beta3b"; // "" for stable Release, e.g. "beta" or "rc.1.2" for Prerelease. // Also used for NuGet Version.
+        public const string VersionPrerelease = "beta4"; // "" for stable Release, e.g. "beta" or "rc.1.2" for Prerelease. // Also used for NuGet Version.
 
 #if DEBUG
         /// <summary>
