c51820be576ca6bffc687e679ef9e2c7b109f824
Ben Callaghan <bcallaghan@etogy.com>
2018-10-31 10:40:22 -0600

    Disabled the debug-mode IV

--- src/PdfSharp/Pdf.Security/PdfStandardSecurityHandler.cs
+++ src/PdfSharp/Pdf.Security/PdfStandardSecurityHandler.cs
@@ -539,7 +539,7 @@ namespace PdfSharp.Pdf.Security
                 aes.KeySize = _keySize * 8;
                 aes.Key = _key;
                 // Enable for debugging only! Provides a consistent IV when testing the encryption
-                aes.IV = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+                // aes.IV = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
                 using (ICryptoTransform encryptor = aes.CreateEncryptor())
                 {
                     byte[] encrypted = encryptor.TransformFinalBlock(data, 0, data.Length);
