aba9c2bddb08d5b9bb1ce01737b5c27ce09ea24e
Martin Ossendorf <Martin.Ossendorf@empira.de>
2019-03-14 16:48:11 +0100

    Updated Copyrights

--- src/PdfSharp/Drawing/XRadialGradientBrush.cs
+++ src/PdfSharp/Drawing/XRadialGradientBrush.cs
@@ -4,7 +4,7 @@
 // Authors:
 //   Stefan Lange (mailto:Stefan.Lange@pdfsharp.com)
 //
-// Copyright (c) 2005-2009 empira Software GmbH, Cologne (Germany)
+// Copyright (c) 2005-2019 empira Software GmbH, Cologne Area (Germany)
 //
 // http://www.pdfsharp.com
 // http://sourceforge.net/projects/pdfsharp
