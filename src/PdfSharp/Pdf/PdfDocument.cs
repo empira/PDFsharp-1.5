4ab4fe4bb74cb23b29ab7f8c8f2ced1c686cd38c
Ian Sinke <ian.n.sinke@gmail.com>
2020-04-13 18:38:38 -0400

    Allow changing PDF document producer

--- src/PdfSharp/Pdf/PdfDocument.cs
+++ src/PdfSharp/Pdf/PdfDocument.cs
@@ -462,17 +462,9 @@ namespace PdfSharp.Pdf
             if (info.Elements[PdfDocumentInformation.Keys.Creator] == null)
                 info.Creator = pdfSharpProducer;
 
-            // Keep original producer if file was imported.
-            string producer = info.Producer;
-            if (producer.Length == 0)
-                producer = pdfSharpProducer;
-            else
-            {
-                // Prevent endless concatenation if file is edited with PDFsharp more than once.
-                if (!producer.StartsWith(VersionInfo.Title))
-                    producer = pdfSharpProducer + " (Original: " + producer + ")";
-            }
-            info.Elements.SetString(PdfDocumentInformation.Keys.Producer, producer);
+            // Set Producer if value is undefined
+            if (info.Elements[PdfDocumentInformation.Keys.Producer] == null)
+                info.Producer = VersionInfo.Producer;
 
             // Prepare used fonts.
             if (_fontTable != null)
